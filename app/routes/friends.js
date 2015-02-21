5a918561360b058e1384bafecce86a60b639ab37
Brian Foshee <brianfoshee@gmail.com>
2015-02-21 14:07:41 -0500

    destroy friend functionality

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -9,6 +9,13 @@ export default Ember.Route.extend({
     cancel: function() {
       console.log("+- cancel action in friends route");
       return true;
+    },
+    delete: function(friend) {
+      var _this = this;
+
+      friend.destroyRecord().then(function(){
+        _this.transitionTo('friends.index');
+      });
     }
   }
 });
