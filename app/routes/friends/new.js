8980f2f9176bd4ba0044847f4dc04ef44bf0ab27
Brian Foshee <brianfoshee@gmail.com>
2015-02-21 20:40:02 -0500

    cleanup

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -4,14 +4,12 @@ export default Ember.Route.extend({
   model: function() {
     return this.store.createRecord('friend');
   },
-  actions: {
-    save: function() {
-      console.log('+- save action in friends new route');
-      return true;
-    },
-    cancel: function() {
-      console.log("+- cancel action in friends new route");
-      return true;
+  deactivate: function(){
+    var model = this.modelFor('friends/new');
+
+    // if this hasen't been saved to the backend, destroy it
+    if (model.get('isNew')) {
+      model.destroyRecord();
     }
   }
 });
