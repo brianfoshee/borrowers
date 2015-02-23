8ee7057a3edfced6ebd52c7382e518e62017aebc
Brian Foshee <brianfoshee@gmail.com>
2015-02-22 22:49:56 -0500

    transition to articles route on cancel

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -2,14 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
-    save: function() {
-      console.log('+- save action in friends route');
-      return false;
-    },
-    cancel: function() {
-      console.log("+- cancel action in friends route");
-      return true;
-    },
     delete: function(friend) {
       var _this = this;
 
